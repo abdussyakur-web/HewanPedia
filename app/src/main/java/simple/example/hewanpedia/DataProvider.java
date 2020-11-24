cf2cdce3cae0141ee9b95bc6d9709fb779a66f0a
hikmatullohwahyuli <hikmatullahwahyuli@gmail.com>
2020-11-24 08:39:35 +0700

    Hewan Pedia

---
+++ app/src/main/java/simple/example/hewanpedia/DataProvider.java
@@ -0,0 +1,93 @@
+package simple.example.hewanpedia;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import simple.example.hewanpedia.model.Anjing;
+import simple.example.hewanpedia.model.Buaya;
+import simple.example.hewanpedia.model.Hewan;
+import simple.example.hewanpedia.model.Kucing;
+
+public class DataProvider {
+    private static List<Hewan> hewans = new ArrayList<>();
+
+    private static List<Buaya> intDataBuaya(Context ctx) {
+        List<Buaya> buayas = new  ArrayList<>();
+        buayas.add(new Buaya("Buaya muara", "indonesia dan australia",
+                "Buaya yang besar.ukurannya bisa mencapai 7 meter - 8 meter dengan berat 200 kilogram. buaya ini biasa hidup di hulu sungai hingga kelaut.buaya muara adalah buaya terganas yang ada di indonesia dan australia",
+                R.drawable.buaya_muara));
+        buayas.add(new Buaya("Buaya siam","indonesia,malasyia,thailand dan kamboja",
+                "Namanya buaya siam tapi orang jawa menyebutnya buaya kodok, buaya yang hidup di jawa dan kalimantan ini ukurannya cukup kecil,haya 2-3 meter. Buaya siam terbesar haya 4 meter.Buaya siam biasanya tinggal di perairan tenang,seperti rawa dan sungai.",
+                R.drawable.buaya_siam));
+        buayas.add(new Buaya("Buaya irian","Irian",
+                " sesuai dengan namanya,buaya ini hidup di perairan tawar irian.secara fisik,buaya irian mirip dengan buaya muara. ukuran tubuh buaya irian bisa mencapai 3,3 meter, sedangkan ukuran buaya irian betina ukuran tubuhnya bisa mencapi 2,6 meter.",
+                R.drawable.buaya_irian));
+        buayas.add(new Buaya("Buaya sapit","sumatra",
+                " Tubuh buaya sapit/ senyulong memang mirip dengan buaya .tetapi, hewan ini memiliki genus yang berbeda dengan buaya. Buaya sepit punya moncong yang runcing dan kecil. lalu tubuh bagaian atasnya tidak terlalu berseri, seperti buaya pada umumnya",
+                R.drawable.buaya_sapit));
+        return buayas;
+    }
+
+    private static List<Anjing> initDataAnjing(Context ctx) {
+        List<Anjing> anjings = new ArrayList<>();
+        anjings.add(new Anjing("Bulldog", "Inggris",
+                "Anjing populer yang dikenal dengan badan yang gemuk berotot, wajah seperti kain kusut, dan hidung yang pesek", R.drawable.dog_bulldog));
+        anjings.add(new Anjing("Husky", "Alaska,Siberia,Finlandia (daerah bersalju) ",
+                "Anjing jenis khusus yang awalnya digunakan untuk menarik kereta salju", R.drawable.dog_husky));
+        anjings.add(new Anjing("Kintamani", "Indonesia",
+                "Ras anjing asli pulau Bali ", R.drawable.dog_kintamani));
+        anjings.add(new Anjing("Samoyed", "Rusia",
+                "Anjing yang berasal dari Siberia yang awalnya biasa digunakan untuk menjaga ternak oleh suku Samoyed", R.drawable.dog_samoyed));
+        anjings.add(new Anjing("Shepherd", "Jerman",
+                "Anjing pintar dan kuat, sekarang banyak digunakan sebagai anjing penjaga, pengawal, dan anjing polisi (K9)", R.drawable.dog_shepherd));
+        anjings.add(new Anjing("Shiba", "Jepang",
+                "Anjing dari daerah Shiba ini gesit dan lincah sehingga awalnya banyak digunakan untuk berburu ", R.drawable.dog_shiba));
+        return anjings;
+    }
+
+    private static List<Kucing> initDataKucing(Context ctx) {
+        List<Kucing> kucings = new ArrayList<>();
+        kucings.add(new Kucing("Angora", "Turki",
+                "Aslinya berasal dari Ankara, Turki dengan ciri khas berbulu panjang dan lembut", R.drawable.cat_angora));
+        kucings.add(new Kucing("Bengal", "Inggris",
+                "Merupakan hasil persilangan antara Kucing Leopard Asia dengan kucing domestik Inggris", R.drawable.cat_bengal));
+        kucings.add(new Kucing("Birmani", "Birma/Myanmar",
+                "Konon awalnya merupakan ras kucing yand biasa menemani para biksu yang menetap pada kuil di Birma", R.drawable.cat_birman));
+        kucings.add(new Kucing("Persia", "Iran",
+                "Kucing berbulu panjang dengan wajah bundar dan hidung pesek. Awalnya berasal dari Iran kemudian diimpor ke Italia", R.drawable.cat_persia));
+        kucings.add(new Kucing("Siam", "Thailand",
+                "Kucing hasil turunan dari Wichian Maat (Kucing Thailand) dengan ciri khas berbadan panjang dan langsing, wajah lonjong, telinga lebar, dan mata kebiruan", R.drawable.cat_siam));
+        kucings.add(new Kucing("Siberia", "Rusia",
+                "Kucing domestik Rusia dengan bulu lebat dan badan besar tapi sangat lincah dan terkenal dengan kemampuan melompatnya ", R.drawable.cat_siberian));
+        return kucings;
+    }
+
+    private static void initAllHewans(Context ctx) {
+        hewans.addAll(initDataKucing(ctx));
+        hewans.addAll(initDataAnjing(ctx));
+        hewans.addAll(intDataBuaya(ctx));
+    }
+
+    public static List<Hewan> getAllHewan(Context ctx) {
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        return  hewans;
+    }
+
+    public static List<Hewan> getHewansByTipe(Context ctx, String jenis) {
+        List<Hewan> hewansByType = new ArrayList<>();
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        for (Hewan h : hewans) {
+            if (h.getJenis().equals(jenis)) {
+                hewansByType.add(h);
+            }
+        }
+        return hewansByType;
+    }
+
+}
